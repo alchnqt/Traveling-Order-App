a0a0e8d6eb460349fd71b21883edeb2b616b7e91
alex <wonderfuljinx@gmail.com>
2021-03-29 16:47:37 +0300

    mvvm

--- FakeAtlas/ViewModels/LoginViewModel.cs
+++ FakeAtlas/ViewModels/LoginViewModel.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using FakeAtlas.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FakeAtlas.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : ObjectViewModel<Account>
     {
+        public Account SelectedAccount
+        {
+            get { return _objectViewModel; }
+            set
+            {
+                _objectViewModel = value;
+                OnPropertyChanged("SelectedAccount");
+            }
+        }
     }
 }
